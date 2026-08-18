d3d907ca09516f6f10a51b111b46d421dc5c8f0f
Mikhail <mishaspb123@gmail.com>
2026-08-18 14:40:26 +0300

    läksy

---
+++ Läksy 2.1.py
@@ -0,0 +1,2 @@
+name = input("Anna nimi: ")
+print ("Terve," + name + "!")
---
+++ Läksy 2.4.py
@@ -0,0 +1,11 @@
+luku1 = int(input("Anna luku1: "))
+luku2 = int(input("Anna luku2: "))
+luku3 = int(input("Anna luku3: "))
+
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
+
+print(f"Summa: {summa}")
+print(f"Tulo: {tulo}")
+print(f"Keskiarvo: {keskiarvo}")
